5c9f862dc517bff90308a182248705b8e5ec9f92
Fero9 <felixmulti00@gmail.com>
2023-06-19 14:07:03 +0200

    Added more checkbox tests

--- cypress/e2e/checkbox-demo.cy.js
+++ cypress/e2e/checkbox-demo.cy.js
@@ -1,11 +1,41 @@
 beforeEach(() => {
     cy.visit('https://www.lambdatest.com/selenium-playground/')
+    cy.contains('Checkbox Demo').click()
   })
 
 describe('All of the Checkbox tests', () => {
-it('Enters a message inside the "Enter Message" field', () => {
-    cy.contains('Checkbox Demo').click()
+it('Selects/deselects the checkbox', () => {
+    cy.get('#isAgeSelected').click()
+    cy.get('#isAgeSelected').should('be.checked')
 
+    cy.get('#txtAge').should('be.visible')
+                     .and('contain', 'Checked')
+
+    cy.get('#isAgeSelected').click()
+    cy.get('#isAgeSelected').should('not.be.checked')
 })
 
+it('Tests the disabled checkboxes', () =>{
+  cy.get(':nth-child(2) > :nth-child(1) > .mr-10').should('be.enabled')
+  cy.get(':nth-child(2) > :nth-child(2) > .mr-10').should('be.enabled')
+  cy.get(':nth-child(2) > :nth-child(3) > .mr-10').should('be.disabled')
+  cy.get(':nth-child(2) > :nth-child(4) > .mr-10').should('be.disabled')
+
+  cy.get(':nth-child(2) > :nth-child(1) > .mr-10').click().should('be.checked')
+  cy.get(':nth-child(2) > :nth-child(2) > .mr-10').click().should('be.checked')
+})
+
+it('Selects/deselects all of the options', () =>{
+  cy.get('#box').click()
+  cy.get('#ex1-check1').should('be.checked')
+  cy.get('#ex1-check2').should('be.checked')
+  cy.get(':nth-child(3) > #ex1-check3').should('be.checked')
+  cy.get(':nth-child(4) > #ex1-check3').should('be.checked')
+
+  cy.get('#box').click()
+  cy.get('#ex1-check1').should('not.be.checked')
+  cy.get('#ex1-check2').should('not.be.checked')
+  cy.get(':nth-child(3) > #ex1-check3').should('not.be.checked')
+  cy.get(':nth-child(4) > #ex1-check3').should('not.be.checked')
+})
 })
